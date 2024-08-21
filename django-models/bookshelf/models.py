061ad36bc6405ce61317ab5054ce11ba6f0c4a94
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-08-21 23:25:26 +0300

    Update models.py

--- django-models/bookshelf/models.py
+++ django-models/bookshelf/models.py
@@ -6,3 +6,14 @@ class Book(models.Model):
     publication_year = models.IntegerField(default=2000)
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
