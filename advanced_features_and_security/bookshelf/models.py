bbe2b528220c78c1cb6140edc92f191a0b719fb6
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-08-23 05:28:43 +0300

    Update models.py

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -6,3 +6,19 @@ class Book(models.Model):
     publication_year = models.IntegerField(default=2000)
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+
+
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view post"),
+            ("can_create", "Can create post"),
+            ("can_edit", "Can edit post"),
+            ("can_delete", "Can delete post"),
+        ]
