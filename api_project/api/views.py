a855c578c602c0318142d6503729ad5a3573641a
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-08-31 21:03:11 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -10,3 +10,13 @@ class BookList(ListAPIView):
 
 "generics.ListAPIView"
 
+
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
