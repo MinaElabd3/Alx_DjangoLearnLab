5528e679947ccf7a904a2494e05a651a0a36bb52
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-08-31 20:49:27 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -11,7 +11,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Ensure this line includes your app's urls.py
+    path('api/', include("api.urls")),  # Ensure this line includes your app's urls.py
 ]
 
 
