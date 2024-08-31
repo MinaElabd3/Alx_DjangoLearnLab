fe3199186ef17ba609289db8d03c12dba9cc1ada
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-08-31 21:04:44 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -23,3 +23,15 @@ urlpatterns = [
     path('api/', include("api.urls")),
     path('', lambda request: HttpResponse('It works!')),
 ]
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
