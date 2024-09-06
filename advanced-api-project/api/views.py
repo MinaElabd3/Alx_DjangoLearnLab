d1ade6b5117c1067df55a95c1cef00161d0e5fdb
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-09-06 03:12:33 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -42,3 +42,14 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     def perform_update(self, serializer):
         # Custom behavior before updating a book
         serializer.save()
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author', 'publication_year']  # fields to filter by
