40fb65f852ea56c534357dffdffefc134e1e68db
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-09-13 02:04:56 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.email = request.POST.get('email', user.email)
+        user.save()
+        return redirect('profile')
+    return render(request, 'registration/profile.html')
+  
