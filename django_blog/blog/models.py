0ef52d5ea24267750c02b5fdc9101cfb53746548
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-09-13 02:23:55 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -11,3 +11,16 @@ class Post(models.Model):
         return self.title
 
 # Create your models here.
+
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    # For profile picture:
+    # profile_picture = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
