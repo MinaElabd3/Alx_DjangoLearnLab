e108c34176b758c570edd027ba4d331e98a1f5bd
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-09-13 22:56:44 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,31 +1,37 @@
-# blog/forms.py
-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Post, Comment, Tag
+from taggit.forms import TagField, TagWidget  # Import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ['username', 'email', 'password1', 'password2']
 
 
-from django import forms
-from .models import Comment
+class PostForm(forms.ModelForm):
+    tags = TagField(required=False)  # Use TagField for tags
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']  # Include tags in the fields
+        widgets = {
+            'tags': TagWidget(),  # Specify the TagWidget for the tags field
+        }
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
 
-from django import forms
-from .models import Post
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'tags']
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if not content or content.isspace():
+            raise forms.ValidationError("Comment cannot be empty")
+        return content
 
 
