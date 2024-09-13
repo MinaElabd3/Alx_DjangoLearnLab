ad9af903e176332d86daca2cbc024c5c23a0ed24
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-09-13 22:48:31 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -135,3 +135,21 @@ def CommentDeleteView(request, comment_id):
         comment.delete()
     return redirect('post_detail', id=comment.post.id)
 
+
+from django.db.models import Q
+from django.shortcuts import render
+from .models import Post
+
+def search_posts(request):
+    query = request.GET.get('q')
+    results = Post.objects.none()
+
+    if query:
+        results = Post.objects.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(tags__name__icontains=query)
+        ).distinct()
+
+    return render(request, 'blog/search_results.html', {'results': results, 'query': query})
+
