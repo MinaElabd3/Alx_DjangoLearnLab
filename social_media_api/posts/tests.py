71f79e090283e7f03d67792da417fbca8cff9905
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-09-21 20:52:10 +0300

    Update tests.py

--- social_media_api/posts/tests.py
+++ social_media_api/posts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Post, Comment
+
+User = get_user_model()
+
+class PostTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.login(username='testuser', password='testpass')
+
+    def test_create_post(self):
+        response = self.client.post('/api/posts/', {'title': 'New Post', 'content': 'Post content'})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list_posts(self):
+        response = self.client.get('/api/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+# Similar tests can be written for comments
 
 # Create your tests here.
