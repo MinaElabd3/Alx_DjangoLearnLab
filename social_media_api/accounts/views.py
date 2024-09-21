30dee2fc3b9803557a0769cdc7f514639424f5fb
Mina Elabd <133127375+MinaElabd3@users.noreply.github.com>
2024-09-21 20:57:23 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -59,3 +59,18 @@ def unfollow_user(request, user_id):
     user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
     request.user.following.remove(user_to_unfollow)
     return Response({'message': f'You have unfollowed {user_to_unfollow.username}'})
+
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Post
+from accounts.models import CustomUser
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_feed(request):
+    followed_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    serialized_posts = PostSerializer(posts, many=True)  # Assuming you have a PostSerializer
+    return Response(serialized_posts.data)
